32f59f0cbc67b48cda71f324e2735ae06223c3f1
Joshua Kimbareeba <jkimbareeba@yahoo.com>
2021-12-12 16:15:50 +0300

    initialization

--- App.js
+++ App.js
@@ -1,21 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import IndexScreen from "./src/screens/IndexScreen";
+import { BlogProvider } from "./src/context/BlogContext";
 
-export default function App() {
+const Navigator = createStackNavigator();
+
+export default () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <BlogProvider>
+      <NavigationContainer>
+        <Navigator.Navigator>
+          <Navigator.Screen name="Blog" component={IndexScreen} />
+        </Navigator.Navigator>
+      </NavigationContainer>
+    </BlogProvider>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
