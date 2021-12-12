bbc6744c1eabc42f80a3b1f7c6654ac111e5b5ad
Joshua Kimbareeba <jkimbareeba@yahoo.com>
2021-12-13 00:12:12 +0300

    update-create-context-import-files

--- App.js
+++ App.js
@@ -2,18 +2,18 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import IndexScreen from "./src/screens/IndexScreen";
-import { BlogProvider } from "./src/context/BlogContext";
+import { Provider } from "./src/context/BlogContext";
 
 const Navigator = createStackNavigator();
 
 export default () => {
   return (
-    <BlogProvider>
+    <Provider>
       <NavigationContainer>
         <Navigator.Navigator>
           <Navigator.Screen name="Blog" component={IndexScreen} />
         </Navigator.Navigator>
       </NavigationContainer>
-    </BlogProvider>
+    </Provider>
   );
 };
