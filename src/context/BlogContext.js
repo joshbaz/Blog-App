20f38884368f213fd5e2dcbde72dd21588464669
Joshua Kimbareeba <jkimbareeba@yahoo.com>
2021-12-13 16:16:32 +0300

    create-blog-update-context

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -23,8 +23,9 @@ const blogReducer = (state, action) => {
 
 //actions
 const addBlogPost = (dispatch) => {
-  return (title, content) => {
+  return (title, content, callback) => {
     dispatch({ type: "add_blogpost", payload: { title, content } });
+    callback();
   };
 };
 
