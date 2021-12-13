944b93d613ed9e2f227eac08701a4caa1701f30e
Joshua Kimbareeba <jkimbareeba@yahoo.com>
2021-12-13 16:44:58 +0300

    edit-blog

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,18 +1,30 @@
 import React, { useContext } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import { Context } from "../context/BlogContext";
+import { EvilIcons } from "@expo/vector-icons";
 
-const ShowScreen = ({ navigation,  ...props }) => {
+const ShowScreen = ({ navigation, ...props }) => {
   const { state } = useContext(Context);
-  console.log( props.route.params.id);
+  console.log(props.route.params.id);
   const blogPost = state.find(
     (blogPost) => blogPost.id === props.route.params.id
   );
   return (
     <View>
       <Text>{blogPost.title}</Text>
+      <Text>{blogPost.content}</Text>
     </View>
   );
+};
+
+ShowScreen.navigationOptions = ({ navigation }) => {
+  return {
+    headerRight: (
+      <TouchableOpacity onPress={() => navigation.navigate("Edit")}>
+        <EvilIcons name="pencil" size={35} />
+      </TouchableOpacity>
+    ),
+  };
 };
 
 const styles = StyleSheet.create({});
